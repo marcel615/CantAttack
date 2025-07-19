44219cb32e065a789f41016bc4ba3500813c19e4
MinjaePark <alswo000615@gmail.com>
2025-07-19 14:54:54 +0900

    feat: UIManager 루트 오브젝트 생성 및 하위 UI 연결 자동 할당 기능 추가
    - UIManager 루트 오브젝트 생성 및 스크립트 연결
    - 인스펙터에서 하위 UI 오브젝트 수동 연결 지원
    - 누락된 연결은 Awake()에서 자동 탐색 후 할당하도록 구현

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    //자식 오브젝트 (UI 오브젝트)
+    //Canvas
+    public GameObject UICanvas;
+    //HPPanel
+    public GameObject HPPanel;
+    public GameObject Portrait;
+    public GameObject HPContainer;
+    //??Panel
+
+
+
+    private void Awake()
+    {
+        //인스펙터에서 연결 까먹었을 경우에 대비
+        if (UICanvas == null) UICanvas = transform.Find("UICanvas")?.gameObject;
+        if (HPPanel == null) HPPanel = transform.Find("UICanvas/HPPanel")?.gameObject;
+        if (Portrait == null) Portrait = transform.Find("UICanvas/HPPanel/Portrait")?.gameObject;
+        if (HPContainer == null) HPContainer = transform.Find("UICanvas/HPPanel/HPContainer")?.gameObject;
+
+
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
