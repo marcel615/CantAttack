9eec034fa9605349bc2ab201e129649ae750ee1a
MinjaePark <alswo000615@gmail.com>
2025-07-19 15:24:02 +0900

    feat: GameManager에 UIManager 배치 및 단일 싱글턴 패턴 적용
    - UIManager를 GameManager 하위에 배치
    - 인스펙터 연결 및 null 체크 후 코드 내 할당 처리
    - 단일 싱글턴 패턴 적용하여 전역 인스턴스 관리

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    //Manager들 및 자주 참조하는 오브젝트들 연결
+    public UIManager UIManager;
+    public Player Player;
+
+
+
+    private void Awake()
+    {
+        //싱글턴 패턴 생성
+        // 기존 GameManager 인스턴스가 존재할 때 && 지금 새로운 GameManager 인스턴스가 생성되려고 할 때
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);    //중복되지 않도록 지금 새롭게 생성되는 놈은 파괴시킨다
+            return;
+        }
+        // GameManager 인스턴스 처음 할당할 때
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        
+    }
+
+    void Start()
+    {
+        //인스펙터에서 연결 까먹었을 경우에 대비
+        if (UIManager == null) UIManager = transform.Find("UIManager")?.GetComponent<UIManager>();
+        if (Player == null) Player = GameObject.Find("Player")?.GetComponent<Player>();
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
