cca66031173225a447212e958fba6a4b083434e2
MinjaePark <alswo000615@gmail.com>
2025-07-23 14:37:17 +0900

    feat: MenuUI 제작
    - MenuPanel에 화면 전체 검은색 반투명 배경 추가
    - MenuSelectPanel에 Continue, Setting, SaveAndExit 버튼 배치
    - 버튼 간 키보드 및 마우스 선택과 이동 기능 설정

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
@@ -15,7 +16,12 @@ public class UIManager : MonoBehaviour
     public GameObject HPPanel;
     public GameObject Portrait;
     public GameObject HPContainer;
-    //??Panel
+    //MenuPanel
+    public GameObject MenuPanel;
+    //public GameObject MenuSelectPanel;
+    public GameObject ContinueButton;
+    public GameObject SettingButton;
+    public GameObject SaveAndExitButton;
 
 
 
@@ -33,11 +39,25 @@ public class UIManager : MonoBehaviour
 
         //자식 오브젝트들 인스펙터에서 연결 까먹었을 경우에 대비
         if (UICanvas == null) UICanvas = transform.Find("UICanvas")?.gameObject;
+
         if (HPPanel == null) HPPanel = transform.Find("UICanvas/HPPanel")?.gameObject;
         if (Portrait == null) Portrait = transform.Find("UICanvas/HPPanel/Portrait")?.gameObject;
         if (HPContainer == null) HPContainer = transform.Find("UICanvas/HPPanel/HPContainer")?.gameObject;
 
+        if (MenuPanel == null) MenuPanel = transform.Find("UICanvas/MenuPanel")?.gameObject;
+        //if (MenuSelectPanel == null) MenuSelectPanel = transform.Find("UICanvas/MenuPanel/MenuSelectPanel")?.gameObject;
+        if (ContinueButton == null) ContinueButton = transform.Find("UICanvas/MenuPanel/ContinueButton")?.gameObject;
+        if (SettingButton == null) SettingButton = transform.Find("UICanvas/MenuPanel/SettingButton")?.gameObject;
+        if (SaveAndExitButton == null) SaveAndExitButton = transform.Find("UICanvas/MenuPanel/SaveAndExitButton")?.gameObject;
+
 
     }
+    private void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(ContinueButton);
+
+    }
+
 
 }
