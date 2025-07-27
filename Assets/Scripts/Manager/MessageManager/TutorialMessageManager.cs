86ae7bfa30b362531952adde03374483ddf0841c
MinjaePark <alswo000615@gmail.com>
2025-07-28 01:04:00 +0900

    feat: 튜토리얼 메시지 시스템 기본 구조 구현
    - TutorialMessageSO: messageID와 메시지 저장, Dialogue/TutorialMessage 폴더에 SO 저장
    - TutorialMessageManager: 메시지 ID와 내용 딕셔너리 관리, GetMessage(id) 메서드 구현
    - TutorialMessageController: 상황에 맞게 튜토리얼 메시지 요청 처리
    - MessageManager: TutorialMessageManager 컴포넌트 부착, 자식 오브젝트에 MessageController 생성 및 TutorialMessageController 부착
    - DialoguePanel: 대사창 UI 패널 생성 및 Dialogue.cs 스크립트 부착
    - DialogueInputController: UIManager에 부착, 대화창 인풋 요청 처리 담당

---
+++ Assets/Scripts/Manager/MessageManager/TutorialMessageManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMessageManager : MonoBehaviour
+{
+    //오브젝트 중복체크를 위한 인스턴스 생성
+    public static TutorialMessageManager Instance;
+
+    //등록된 튜토리얼 메시지들
+    public List<TutorialMessageSO> tutorialMessages;
+    //메시지들로 딕셔너리 만들기
+    private Dictionary<string, string> messageDict;
+
+
+    private void Awake()
+    {
+        // 기존 인스턴스가 존재할 때 && 지금 새로운 인스턴스가 생성되려고 할 때
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);    //중복되지 않도록 지금 새롭게 생성되는 놈은 파괴시킨다
+            return;
+        }
+        // 인스턴스 처음 할당
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        messageDict = new Dictionary<string, string>();
+        GetMessageDic();
+
+    }
+    void GetMessageDic()
+    {
+        foreach (var msg in tutorialMessages)
+        {
+            if (!messageDict.ContainsKey(msg.messageID))
+                messageDict.Add(msg.messageID, msg.messageKor);
+        }
+    }
+    public string GetMessage(string msgID)
+    {
+        if (messageDict.ContainsKey(msgID))
+        {
+            return messageDict[msgID];
+        }
+        else
+        {
+            return $"[튜토리얼 메시지 없음: {msgID}]";
+        }
+    }
+
+}
---
+++ Assets/Scripts/ScriptableObject/TutorialMessageSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TutorialMessage")]
+public class TutorialMessageSO : ScriptableObject
+{
+    [Header("식별용 ID (코드에서 호출 시 사용)")]
+    public string messageID;
+
+    [Header("메시지 내용")]
+    public string messageKor;
+}
