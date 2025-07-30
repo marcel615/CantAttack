95aff040b09c1098b6c02659bd55bc077ce2fac1
MinjaePark <alswo000615@gmail.com>
2025-07-30 21:42:31 +0900

    feat: MessageSO 메시지 필드에 TextArea 속성 추가하여 줄바꿈 입력 지원

--- Assets/Scripts/ScriptableObject/TutorialMessageSO.cs
+++ Assets/Scripts/ScriptableObject/TutorialMessageSO.cs
@@ -8,6 +8,7 @@ public class TutorialMessageSO : ScriptableObject
     [Header("식별용 ID (코드에서 호출 시 사용)")]
     public string messageID;
 
+    [TextArea]
     [Header("메시지 내용")]
     public string messageKor;
 }
