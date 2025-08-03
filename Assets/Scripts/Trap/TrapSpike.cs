65649ca97fae86bd8dfa700da242b7948bbc47bb
MinjaePark <alswo000615@gmail.com>
2025-08-03 21:01:24 +0900

    feat: 가시 장애물 구현 및 데미지 처리 로직 추가
    - TrapSpike.cs 생성: 트리거 충돌 시 IDamageable 구현 객체에게 TakeDamage() 호출
    - 트리거 기반 가시 장애물 데미지 처리 로직 구현
    - 장애물 이미지로 무료 에셋 활용

--- Assets/Scripts/Trap/TrapSpike.cs
+++ Assets/Scripts/Trap/TrapSpike.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrapSaw : MonoBehaviour
+public class TrapSpike : MonoBehaviour
 {
     //내 컴포넌트
-    Rigidbody2D rigid;
-    CircleCollider2D damageCollider;
+    BoxCollider2D damageCollider;
 
     //기본 변수
     [SerializeField] private int damage = 1;
@@ -14,19 +13,16 @@ public class TrapSaw : MonoBehaviour
 
     private void Awake()
     {
-        rigid = GetComponent<Rigidbody2D>();
-        damageCollider = GetComponent<CircleCollider2D>();
-        
+        damageCollider = GetComponent<BoxCollider2D>();
     }
 
     //플레이어 및 적들과의 충돌 시 로직 구현
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.TryGetComponent<IDamageable>(out IDamageable target))
+        if (collision.TryGetComponent<IDamageable>(out IDamageable target))
         {
             target.TakeDamage(transform.position, damage);
         }
     }
-
 
 }
