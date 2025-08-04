1d8a81de714542dec17fb862394d37a6a7f0648e
MinjaePark <alswo000615@gmail.com>
2025-08-04 21:07:00 +0900

    feat: 낙하형 장애물 CrusherBlock 등속 낙하/복귀 로직 구현
    - TrapBody 레이어 추가 및 Collision Matrix 설정 (Ground와만 충돌)
    - CrusherBlock 본체 레이어를 TrapBody로 지정
    - TrapCrusherBlock.cs에서 등속 이동 기반 낙하 및 복귀 로직 구현
        - 일정 위치 도달 시 코루틴으로 대기 후 낙하/복귀
        - 낙하 및 복귀 중 데미지 콜라이더 on/off 처리

--- Assets/Scripts/Trap/TrapCrusher/TrapCrusherBlock.cs
+++ Assets/Scripts/Trap/TrapCrusher/TrapCrusherBlock.cs
@@ -1,71 +1,99 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class TrapCrusherBlock : MonoBehaviour
 {
-    //내 컴포넌트
-    Rigidbody2D rigid;
-    BoxCollider2D bodyCollider;
-
     //자식 오브젝트
     [SerializeField] private GameObject damageCollider;
     [SerializeField] private GameObject groundCollider;
-
+    [SerializeField] private GameObject dropPosition;
 
     //기본 변수
-    [SerializeField] private int damage = 1;
-
-    //콜라이더
+    public int damage = 1;
 
     //움직이는 로직 관련 변수
-    Vector3 originPos;
-    bool isMoveCycleOver;
-    Vector2 RePositionDir;
-    float ResetSpeed = 3f;
+    float waitTime = 1f;        //각 행동을 마치고 기다리는 시간
+    Vector3 originPos;          //떨어지기 전 원래 위치
+    Vector2 dropPos;            //떨어질 곳 위치
+    Vector2 targetPos;          //각 상태에 맞추어 지정할 위치
+    float ResetSpeed = 3f;      //다시 원래 위치로 리셋할 때의 스피드
+    float dropSpeed = 15f;      //떨어지는 스피드
+    float targetSpeed;          //각 상태에 맞추어 지정할 스피드
+
+    //플래그
+    bool isResetAndWait;        //리셋되고 떨어지기를 기다리는 시간인지 플래그
+    bool isDropedAndWait;       //떨어지고 리셋되기를 기다리는 시간인지 플래그
+    bool isStartMove;           //다 기다리고 움직이는 시간인지 플래그
 
 
     private void Awake()
     {
-        rigid = GetComponent<Rigidbody2D>();
-        bodyCollider = GetComponent<BoxCollider2D>();
-
+        //자식 오브젝트들 인스펙터에서 연결 까먹었을 경우에 대비
         if (damageCollider == null) damageCollider = transform.Find("DamageArea")?.gameObject;
-        if (groundCollider == null) groundCollider = transform.Find("GroundArea")?.gameObject;
+        if (dropPosition == null) dropPosition = transform.Find("DropPosition")?.gameObject;
 
+        //각각의 Position 초기화
         originPos = transform.position;
+        dropPos = dropPosition.transform.position;
     }
     private void FixedUpdate()
     {
-        if (!isMoveCycleOver && rigid.velocity.y == 0)
+        //기다렸다가 떨어지기 시작
+        if(!isResetAndWait && Vector3.Distance(transform.position, originPos) < 0.01f)
         {
-            isMoveCycleOver = true;
-
-            //bodyCollider.enabled = true;
-            damageCollider.GetComponent<BoxCollider2D>().enabled = false;
-
-            ResetPosition();
+            StartCoroutine(WaitAndDrop(waitTime));
         }
-        if (isMoveCycleOver)
+        //기다렸다가 원래 위치로 리셋
+        if(!isDropedAndWait && Vector3.Distance(transform.position, dropPos) < 0.01f)
         {
-            if (transform.position.y < originPos.y)
-            {
-                rigid.velocity = RePositionDir * ResetSpeed;
-            }
-            else
-            {
-                isMoveCycleOver = false;
-
-                //bodyCollider.enabled = false;
-                damageCollider.GetComponent<BoxCollider2D>().enabled = true;
-            }
+            StartCoroutine(WaitAndReset(waitTime));
         }
-
+        if (isStartMove)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, targetSpeed * Time.fixedDeltaTime);
+        }
     }
-    void ResetPosition()
+    private IEnumerator WaitAndDrop(float waitTime)
     {
-        RePositionDir = (originPos - transform.position).normalized;
+        //플래그 세팅
+        isResetAndWait = true;
+        isDropedAndWait = false;
+        //움직이기 플래그 세팅
+        isStartMove = false;
+
+        //데미지 콜라이더 잠시 끄고
+        damageCollider.GetComponent<BoxCollider2D>().enabled = false;
+
+        //waitTime만큼 기다렸다가
+        yield return new WaitForSeconds(waitTime);
+
+        //데미지 콜라이더 다시 키고
+        damageCollider.GetComponent<BoxCollider2D>().enabled = true;
+
+        //목표지점, 스피드 설정 후 움직이기 플래그 세팅
+        targetPos = dropPos;
+        targetSpeed = dropSpeed;
+        isStartMove = true;
     }
+    private IEnumerator WaitAndReset(float waitTime)
+    {
+        //플래그 세팅
+        isDropedAndWait = true;
+        isResetAndWait = false;
+        //움직이기 플래그 세팅
+        isStartMove = false;
 
+        //데미지 콜라이더 잠시 끄고
+        damageCollider.GetComponent<BoxCollider2D>().enabled = false;
 
+        //waitTime만큼 기다렸다가
+        yield return new WaitForSeconds(waitTime);
+
+        //목표지점, 스피드 설정 후 움직이기 플래그 세팅
+        targetPos = originPos;
+        targetSpeed = ResetSpeed;
+        isStartMove = true;
+    }
 }
--- Assets/Scripts/Trap/TrapCrusher/TrapCrusherBlockDamagerCollider.cs
+++ Assets/Scripts/Trap/TrapCrusher/TrapCrusherBlockDamagerCollider.cs
@@ -4,25 +4,14 @@ using UnityEngine;
 
 public class TrapCrusherBlockDamagerCollider : MonoBehaviour
 {
-    //내 컴포넌트
-    BoxCollider2D bodyCollider;
-
-    //기본 변수
-    [SerializeField] private int damage = 1;
-
-    private void Awake()
-    {
-        bodyCollider = GetComponent<BoxCollider2D>();
-    }
+    [SerializeField] private TrapCrusherBlock crusherBlock;
 
     //플레이어 및 적들과의 충돌 시 로직 구현
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.TryGetComponent<IDamageable>(out IDamageable target))
         {
-            target.TakeDamage(transform.position, damage);
+            target.TakeDamage(transform.position, crusherBlock.damage);
         }
     }
-
-
 }
