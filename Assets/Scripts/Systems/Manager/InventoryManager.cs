9ba3dead50e6c36164fc7d18a37f522c831ba72f
MinjaePark <alswo000615@gmail.com>
2025-10-31 14:29:40 +0900

    feat(system): 인벤토리 매니저 전역 관리 시스템 구현
    - InventoryManager 오브젝트
        - InventoryManager.cs 부착 및 DontDestroyOnLoad() 적용 → 씬 전환 시 유지
    - InventoryManager.cs
        - 플레이어 획득 아이템 데이터 전역 관리 구조 구현
            - List<ShieldDataSO> shieldInventory / GetShieldInventory()
            - List<ParryModeDataSO> parryModeInventory / GetParryModeInventory()
    
    → 방패, 패리모드 인벤토리를 전역적으로 관리하는 기반 시스템 완성.

---
+++ Assets/Scripts/Systems/Manager/InventoryManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering.LookDev;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    //오브젝트 중복체크를 위한 인스턴스 생성
+    public static InventoryManager Instance;
+
+    //플레이어가 획득한 방패 인벤토리
+    public List<ShieldDataSO> shieldInventory;
+    public IReadOnlyList<ShieldDataSO> GetShieldInventory() => shieldInventory;
+
+    //플레이어가 획득한 패리모드 인벤토리
+    public List<ParryModeDataSO> parryModeInventory;
+    public IReadOnlyList<ParryModeDataSO> GetParryModeInventory() => parryModeInventory;
+
+
+    private void Awake()
+    {
+        // 기존 인스턴스가 존재할 때 && 지금 새로운 인스턴스가 생성되려고 할 때
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);    //중복되지 않도록 지금 새롭게 생성되는 놈은 파괴시킨다
+            return;
+        }
+        // 인스턴스 처음 할당
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+
+
+
+}
